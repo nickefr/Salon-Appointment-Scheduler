34b5ef5f8a6a0a9852190aeb1c452513c5324e07
nickefr <73330890+nickefr@users.noreply.github.com>
2024-04-09 11:44:28 +0300

    Update salon.sql

--- salon.sql
+++ salon.sql
@@ -5,26 +5,26 @@ CREATE DATABASE salon;
 \c salon 
 
 CREATE TABLE IF NOT EXISTS customers (
-salon(>     customer_id SERIAL PRIMARY KEY,
-salon(>     name VARCHAR(255) NOT NULL,
-salon(>     phone VARCHAR(20) UNIQUE NOT NULL
-salon(> );
+    customer_id SERIAL PRIMARY KEY,
+     name VARCHAR(255) NOT NULL,
+     phone VARCHAR(20) UNIQUE NOT NULL
+ );
 CREATE TABLE
 
-salon=> CREATE TABLE IF NOT EXISTS services (
-salon(>     service_id SERIAL PRIMARY KEY,
-salon(>     name VARCHAR(255) NOT NULL
-salon(> );
+ CREATE TABLE IF NOT EXISTS services (
+     service_id SERIAL PRIMARY KEY,
+     name VARCHAR(255) NOT NULL
+ );
 CREATE TABLE
 
-salon=> CREATE TABLE IF NOT EXISTS appointments (
-salon(>     appointment_id SERIAL PRIMARY KEY,
-salon(>     customer_id INT,
-salon(>     service_id INT,
-salon(>     time VARCHAR(100),
-salon(>     FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-salon(>     FOREIGN KEY (service_id) REFERENCES services(service_id)
-salon(> );
+ CREATE TABLE IF NOT EXISTS appointments (
+     appointment_id SERIAL PRIMARY KEY,
+     customer_id INT,
+     service_id INT,
+     time VARCHAR(100),
+     FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+     FOREIGN KEY (service_id) REFERENCES services(service_id)
+);
 CREATE TABLE
 salon=> INSERT INTO services (name) VALUES
 salon->     ('Menu1'),
